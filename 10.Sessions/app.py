0d6550ea6f5bd9fd6cb2c8a58d5c1b1b0297169f
Easton Huang <leihuang007@qq.com>
2023-02-19 22:03:44 +0800

    10.Sessions

---
+++ 10.Sessions/app.py
@@ -0,0 +1,38 @@
+'''
+In addition to the request object there is also a second object called session which allows you to store information specific to a user from one request to the next. 
+This is implemented on top of cookies for you and signs the cookies cryptographically. 
+What this means is that the user could look at the contents of your cookie but not modify it, unless they know the secret key used for signing.
+'''
+from flask import Flask, session, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
